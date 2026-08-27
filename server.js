172038a6c45bbb17d714bedd700ace923dbb7303
Isaias <isaiasagramonte41@gmail.com>
2026-08-27 07:55:10 +0200

    Mostrar interfaz web en el servidor

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const PORT = process.env.PORT || 3000;
 
+// Esto le dice a Express que busque archivos estáticos en la carpeta raíz (o donde tengas tu HTML)
+app.use(express.static(path.join(__dirname)));
+
+// Ruta principal que carga tu archivo de interfaz (ajusta 'index.html' si tu archivo se llama diferente)
 app.get('/', (req, res) => {
-  res.send('¡Servidor de isaias-titan funcionando al 100%!');
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(PORT, () => {
